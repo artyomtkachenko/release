30d57b9c68541d13e50f4818dfe8dcc72009112b
Anthony Waters <anthonywaters@nbnco.com.au>
2016-03-17 16:54:27 +1100

    Added webdav put function

--- src/release/publish/webdav.go
+++ src/release/publish/webdav.go
@@ -1,3 +1,56 @@
 package webdav
 
 //publish to webdav endpoint
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func postFile(filename, targetUrl, username, password string) ([]byte, error) {
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	// this step is very important
+	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
+	check(err)
+
+	// open file handle
+	fh, err := os.Open(filename)
+	check(err)
+
+	//iocopy
+	_, err = io.Copy(fileWriter, fh)
+	check(err)
+
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
+
+	req, err := http.NewRequest("PUT", targetUrl, bodyBuf)
+	check(err)
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.SetBasicAuth(username, password)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	check(err)
+
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	check(err)
+
+	fmt.Println(resp.Status)
+	fmt.Println(string(resp_body))
+
+	return resp_body, nil
+}
--- src/release/publish/webdav_test.go
+++ src/release/publish/webdav_test.go
@@ -1,3 +1,34 @@
 package webdav
 
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
 //publish to webdav endpoint
+
+func Test_check(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	check(errors.New("foobar"))
+}
+
+func Test_postFile_success(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.
+			fmt.Fprintln(w, "Hello Obama")
+	}))
+	defer ts.Close()
+
+	body, _ := sendRequest(ts.URL)
+	if !strings.Contains(string(body), "Hello Obama") {
+		t.Errorf("Expected: body to be Hello, got [%s]\n", body)
+	}
+}
